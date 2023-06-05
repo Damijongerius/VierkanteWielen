1ae2689f62d87cb3a3a8b7a18e81ae4c30c13ba7
SamMayer0815 <sammayer0815@gmail.com>
2023-06-05 13:21:03 +0200

    made all login pages work
    made href work on all pages

--- dist/Index.js
+++ dist/Index.js
@@ -10,4 +10,16 @@ const logger = new Logger_1.Logger("index");
 App_1.app.get("/", function (req, res) {
     res.render("index");
 });
+App_1.app.get("/login", function (req, res) {
+    res.render("login");
+});
+App_1.app.get("/registreer", function (req, res) {
+    res.render("registreer");
+});
+App_1.app.get("/resetWachtwoord", function (req, res) {
+    res.render("resetWachtwoord");
+});
+App_1.app.get("/pakket", function (req, res) {
+    res.render("pakket");
+});
 //# sourceMappingURL=Index.js.map
--- src/Index.ts
+++ src/Index.ts
@@ -1,14 +1,30 @@
 import { Database } from "./DataBase/Database";
-import { UserManager } from './DataBase/UserManager';
+import { UserManager } from "./DataBase/UserManager";
 import { app } from "./App";
-import  path from "path";
+import path from "path";
 import { Logger } from "./Logger";
 
-Database.connect("localhost", "root", "", "vierkantewielen"); 
+Database.connect("localhost", "root", "", "vierkantewielen");
 
-const userManager = new UserManager(); 
+const userManager = new UserManager();
 const logger = new Logger("index");
 
-app.get("/", function(req,res){
-    res.render("index");
-})
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/registreer", function (req, res) {
+  res.render("registreer");
+});
+
+app.get("/resetWachtwoord", function (req, res) {
+  res.render("resetWachtwoord");
+});
+
+app.get("/pakket", function (req, res) {
+  res.render("pakket");
+});
