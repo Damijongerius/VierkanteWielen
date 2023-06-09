8133c4d048e9f0625f821fb6d38a625745704c5b
damij <damianojongerius@gmail.com>
2023-06-09 12:17:49 +0200

    pakket kopen

--- dist/Index.js
+++ dist/Index.js
@@ -12,10 +12,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Database_js_1 = require("./DataBase/Database.js");
 const UserManager_js_1 = require("./DataBase/UserManager.js");
 const App_js_1 = require("./App.js");
+const Subscription_js_1 = require("./manager/Subscription.js");
 const Logger_js_1 = require("./Logger.js");
 const Encryptor_js_1 = require("./encryption/Encryptor.js");
 Database_js_1.Database.connect("localhost", "dami", "dami", "vierkantewielen");
 const userManager = new UserManager_js_1.UserManager();
+const subscriptionManager = new Subscription_js_1.SubscriptionManager();
 const logger = new Logger_js_1.Logger("index");
 const studentPermission = 1;
 App_js_1.app.get("/", function (req, res) {
@@ -30,15 +32,22 @@ App_js_1.app.get("/login", function (req, res) {
             res.render("login");
         }
         else {
-            res.render("rooster");
+            res.redirect("rooster");
         }
     });
+});
+App_js_1.app.get("/dashboard", function (req, res) {
+    res.render("dashboard");
+});
+App_js_1.app.get("/dashboard/autos", function (req, res) {
+    const cars = [];
+    res.render("dashboardCars", cars);
 });
 App_js_1.app.get("/registreer", function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const data = yield App_js_1.redisClient.hGetAll(req.session.id);
         if (data.id != null) {
-            res.render("rooster");
+            res.redirect("rooster");
         }
         else {
             res.render("registreer");
@@ -55,7 +64,7 @@ App_js_1.app.get("/rooster", function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         const data = yield App_js_1.redisClient.hGetAll(req.session.id);
         if (data.id == null) {
-            res.render("login");
+            res.redirect("login");
         }
         else {
             res.render("rooster");
@@ -63,20 +72,24 @@ App_js_1.app.get("/rooster", function (req, res) {
     });
 });
 App_js_1.app.post("/pakket/kopen", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(req.body);
     const data = yield App_js_1.redisClient.hGetAll(req.session.id);
-    if (data.id == null) {
-        res.render("login");
+    if (req.body.bevestigen !== "on") {
+        res.redirect("pakket");
     }
-    //   const { bevestigen12 } = req.body;
-    //   const query = "INSERT INTO subscriptions (subscriptionLevel) VALUES (?)";
-    //   connection.query(query, [bevestigen12], (error, results) => {
-    //     if (error) {
-    //       console.error("Error inserting data:", error);
-    //       res.status(500).json({ error: "An error occurreddddd" });
-    //     } else {
-    //       res.json({ message: "Data inserted successfully" });
-    //     }
-    //   });
+    else if (data.id == null) {
+        res.redirect("login");
+    }
+    else {
+        const id = parseInt(data.id);
+        const result = yield subscriptionManager.subscribe(id, req.body.pakket);
+        if (result != false) {
+            res.redirect("/");
+        }
+        else {
+            res.redirect("pakket");
+        }
+    }
 }));
 App_js_1.app.post("/register", function (req, res) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -84,9 +97,14 @@ App_js_1.app.post("/register", function (req, res) {
         const hashPassword = yield (0, Encryptor_js_1.EncryptPasswordASync)(user.wachtwoord);
         yield userManager
             .addUser(user.voornaam, user.achternaam, user.email, studentPermission, hashPassword, user.tussenvoegsel)
-            .then((result) => {
+            .then((result) => __awaiter(this, void 0, void 0, function* () {
+            yield App_js_1.redisClient.hSet(req.session.id, {
+                email: user.voornaam,
+                id: user.id,
+                permissionLevel: user.studentPermission,
+            });
             res.redirect("rooster");
-        });
+        }));
     });
 });
 App_js_1.app.post("/login", function (req, res) {
@@ -107,7 +125,7 @@ App_js_1.app.post("/login", function (req, res) {
                 yield App_js_1.redisClient.hSet(req.session.id, {
                     email: user.email,
                     id: user.id,
-                    permissionLevel: user.permissionLevel
+                    permissionLevel: user.permissionLevel,
                 });
                 res.redirect("rooster");
             }
@@ -118,9 +136,9 @@ App_js_1.app.post("/login", function (req, res) {
     });
 });
 App_js_1.app.get("/logout", function (req, res) {
-    App_js_1.redisClient.hDel(req.session.id, 'email');
-    App_js_1.redisClient.hDel(req.session.id, 'id');
-    App_js_1.redisClient.hDel(req.session.id, 'permissionLevel');
-    res.redirect('login');
+    App_js_1.redisClient.hDel(req.session.id, "email");
+    App_js_1.redisClient.hDel(req.session.id, "id");
+    App_js_1.redisClient.hDel(req.session.id, "permissionLevel");
+    res.redirect("login");
 });
 //# sourceMappingURL=Index.js.map
---
+++ src/manager/Subscription.ts
@@ -0,0 +1,62 @@
+import { UserManager } from "../DataBase/UserManager";
+
+const userManager : UserManager = new UserManager();
+export class SubscriptionManager{
+    subscription1: Subscription = new Subscription(1,10,true,false,false); 
+    subscription2: Subscription = new Subscription(1,20,true,true,false); 
+    subscription3: Subscription = new Subscription(1,30,true,true,true); 
+
+    async subscribe(subscriber: number, pakket: string){
+        let result: boolean = false;
+        switch(pakket){
+            case 'pakket-1': {
+                result = await this.setSubscription(subscriber, this.subscription1);
+                break;
+            }
+            case 'pakket-2': {
+                result = await this.setSubscription(subscriber, this.subscription2);
+                break;
+            }
+            case 'pakket-3': {
+                result = await this.setSubscription(subscriber, this.subscription3);
+                break;
+            }
+            default: {
+                console.log('pakket not found');
+                break;
+            }
+          }
+          return result;
+    }
+
+    async setSubscription(subscriber: number,subscription: Subscription): Promise<boolean>{
+        const result = await userManager.addSubscription(subscriber,subscription.subscriptieNummer);
+        if(result instanceof Boolean){
+            return false;
+        }else{
+            true;
+        }
+    }
+}
+
+class Subscription{
+    subscriptieNummer: number;
+    lessen: number;
+    examen: boolean;
+    theorieCursus: boolean;
+    tussentijdseToets: boolean;
+     
+    constructor(
+        subscriptieNummer: number,
+        lessen: number,
+        examen: boolean,
+        theorieCursus: boolean,
+        tussentijdseToets: boolean
+    ){
+        this.subscriptieNummer = subscriptieNummer;
+        this.lessen = lessen;
+        this.examen = examen; 
+        this.theorieCursus = theorieCursus;
+        this.tussentijdseToets = tussentijdseToets;
+    }
+}
