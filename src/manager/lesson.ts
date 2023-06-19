b61ae0140dddd5d94b8219cf0e8fa3dc384f20be
SamMayer0815 <sammayer0815@gmail.com>
2023-06-19 09:20:04 +0200

    insert lesson to database done

--- src/manager/lesson.ts
+++ src/manager/lesson.ts
@@ -1,20 +1,50 @@
 import session from "express-session";
 import { redisClient } from "../App";
 import { Request, Response } from "express";
-import { Database } from "../../dist/DataBase.js";
+import { Database } from "../DataBase/Database.js";
 
 export class Lesson {
   async Add(req: Request, res: Response) {
-    if(await hasPermissions(req.session.id) == false){ res.render("/rooster") };
-    
-    const lesson = req.body;
-    lesson.date: Date,
-    lesson.student_name: string,
-    lesson.examen: int,
-    lesson.description: string,
-    lesson.goal: string,
-    lesson.pickup: string,
+    if ((await hasPermissions(req.session.id)) == false) {
+      res.render("/rooster");
+    }
+    console.log(req.body.date);
 
+    const lessonDate = req.body.date;
+    // const student_name = req.body.student_name;
+    const isExam = req.body.examen;
+    const lessonDescription = req.body.description;
+    const lessonGoal = req.body.goal;
+    const ophaalLocatie = req.body.pickup;
+
+    const car_licencePlate = "abc";
+
+    let sqlQuery: string;
+    let values: any[];
+
+    sqlQuery =
+      "INSERT INTO Lessons (lessonDate, lessonDescription, isExam, lessonGoal, ophaalLocatie, car_licencePlate) VALUES (?, ?, ?, ?, ?, ?);";
+    values = [
+      lessonDate,
+      lessonDescription,
+      isExam,
+      lessonGoal,
+      ophaalLocatie,
+      car_licencePlate,
+    ];
+
+    const result: any = await Database.query(sqlQuery, values);
+    console.log(result);
+    return result;
+
+    // [Object: null prototype] {
+    //   date: '2023-06-29T06:00',
+    //   student_name: 'asd',
+    //   examen: '1',
+    //   description: 'asd',
+    //   goal: 'sad',
+    //   pickup: 'location1'
+    // }
   }
 }
 
